a8627261f8059f112acda0708286997bb657f295
mybrokengnome@gmail.com <mybrokengnome@gmail.com@8cab2b82-9bbb-5beb-0cf0-96f62902d034>
2013-04-28 20:15:15 +0000

    Forgot to add in files

---
+++ TT/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Security.Cryptography;
+
+namespace TT
+{
+    public static class Helpers
+    {
+        public static string EncodePassword(string password)
+        {
+            var hash = System.Security.Cryptography.SHA256.Create();
+            var encoder = new System.Text.ASCIIEncoding();
+            var combined = encoder.GetBytes(password ?? "");
+            return BitConverter.ToString(hash.ComputeHash(combined)).ToLower().Replace("-", "");
+        }
+    }
+}
