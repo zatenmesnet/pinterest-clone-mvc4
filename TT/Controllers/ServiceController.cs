3fe8e0047c10f9cce4d0c5c7c04563c0a7270742
mybrokengnome@gmail.com <mybrokengnome@gmail.com@8cab2b82-9bbb-5beb-0cf0-96f62902d034>
2013-04-28 01:00:30 +0000

    Initial commit

---
+++ TT/Controllers/ServiceController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TT.Models;
+
+namespace TT.Controllers
+{
+    public class ServiceController : Controller
+    {
+        //
+        // GET: /Service/
+
+        public ActionResult Index(int id = -1)
+        {
+            if (id == -1)
+                return HttpNotFound();
+
+            var db = new DBModel();
+            return View(db.GetPostCommentsAndUser(id));
+        }
+
+    }
+}
---
+++ TT/Models/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TT.Models
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string filename { get; set; }
+        public int owner { get; set; }
+        public DateTime dateuploaded { get; set; }
+        public int comments_count { get; set; }
+        public int repid_id { get; set; }
+        public int repin_count { get; set; }
+        public int like_count { get; set; }
+    }
+}
